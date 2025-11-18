2b5a76c548093b1c0f2df102403b3a220081f55d
LeoDm123 <leo.meiners1@gmail.com>
2025-11-18 17:04:40 -0300

    cambio de user model

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -10,18 +10,6 @@ const userSchema = Schema({
   personalData: {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    birthDate: { type: Date, required: false },
-    gender: { type: String, required: false },
-    paisCode: { type: String, required: false },
-    phone: { type: String, required: false },
-    nationality: { type: String, required: false },
-    currentCity: { type: String, required: false },
-  },
-
-  socialMedia: {
-    linkedin: { type: String, required: false },
-    instagram: { type: String, required: false },
-    twitter: { type: String, required: false },
   },
   role: {
     type: String,
